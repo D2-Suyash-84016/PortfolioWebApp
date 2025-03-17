8cce5e7edfcfaa695fadc93290e05f295de0eddb
D2_suyash_84016 <patilsuyash1177@gmail.com>
2025-03-17 15:07:24 +0530

    portfolioProjectv3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: "/PortfolioWebApp/",
-=======
+
    base: "/PortfolioWebApp/",
->>>>>>> 8f0c91b28639cd012185934eb5aa32f743bbea6e
+
 })
